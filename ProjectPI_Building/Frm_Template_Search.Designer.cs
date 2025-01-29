f2955e674d02210fe5ed7a5e7e859bb709b3ed2a
AlexMachaca <212129@unamba.edu.pe>
2025-01-29 18:50:39 -0500

    Agregar archivos de proyecto.

---
+++ ProjectPI_Building/Frm_Template_Search.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ProjectPI_Building
+{
+    partial class Frm_Template_Search
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn_salir = new PictureBox();
+            btn_exit = new PictureBox();
+            btn_maximizar = new PictureBox();
+            btn_minimizar = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)btn_salir).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)btn_exit).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)btn_maximizar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)btn_minimizar).BeginInit();
+            SuspendLayout();
+            // 
+            // btn_salir
+            // 
+            btn_salir.BackColor = Color.Transparent;
+            btn_salir.Location = new Point(847, 667);
+            btn_salir.Name = "btn_salir";
+            btn_salir.Size = new Size(153, 67);
+            btn_salir.TabIndex = 0;
+            btn_salir.TabStop = false;
+            btn_salir.Click += btn_salir_Click;
+            // 
+            // btn_exit
+            // 
+            btn_exit.BackColor = Color.Transparent;
+            btn_exit.Location = new Point(977, 0);
+            btn_exit.Name = "btn_exit";
+            btn_exit.Size = new Size(49, 55);
+            btn_exit.TabIndex = 1;
+            btn_exit.TabStop = false;
+            btn_exit.Click += btn_exit_Click;
+            // 
+            // btn_maximizar
+            // 
+            btn_maximizar.BackColor = Color.Transparent;
+            btn_maximizar.Location = new Point(927, 0);
+            btn_maximizar.Name = "btn_maximizar";
+            btn_maximizar.Size = new Size(49, 55);
+            btn_maximizar.TabIndex = 2;
+            btn_maximizar.TabStop = false;
+            // 
+            // btn_minimizar
+            // 
+            btn_minimizar.BackColor = Color.Transparent;
+            btn_minimizar.Location = new Point(872, 0);
+            btn_minimizar.Name = "btn_minimizar";
+            btn_minimizar.Size = new Size(49, 55);
+            btn_minimizar.TabIndex = 3;
+            btn_minimizar.TabStop = false;
+            btn_minimizar.Click += btn_minimizar_Click;
+            // 
+            // Frm_Template_Search
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.Page1;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1026, 762);
+            Controls.Add(btn_minimizar);
+            Controls.Add(btn_maximizar);
+            Controls.Add(btn_exit);
+            Controls.Add(btn_salir);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Frm_Template_Search";
+            Text = "Frm_Template_Search";
+            MouseDown += Frm_Template_Search_MouseDown;
+            MouseMove += Frm_Template_Search_MouseMove;
+            MouseUp += Frm_Template_Search_MouseUp;
+            ((System.ComponentModel.ISupportInitialize)btn_salir).EndInit();
+            ((System.ComponentModel.ISupportInitialize)btn_exit).EndInit();
+            ((System.ComponentModel.ISupportInitialize)btn_maximizar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)btn_minimizar).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox btn_salir;
+        private PictureBox btn_exit;
+        private PictureBox btn_maximizar;
+        private PictureBox btn_minimizar;
+    }
+}
---
+++ ProjectPI_Building/Frm_Template_Search.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectPI_Building
+{
+    public partial class Frm_Template_Search : Form
+    {
+        private bool isDragging = false;
+        private Point startPoint;
+        public Frm_Template_Search()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_Template_Search_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDragging = true;
+                startPoint = new Point(e.X, e.Y);//Registrar el punto de inicio
+            }
+        }
+
+        private void Frm_Template_Search_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+
+                Point newPosition = this.Location;
+                newPosition.X += e.X - startPoint.X;
+                newPosition.Y += e.Y - startPoint.Y;
+                this.Location = newPosition;
+                this.Location = newPosition;//Mueve el formulario
+            }
+        }
+
+        private void Frm_Template_Search_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+        }
+
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_minimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
